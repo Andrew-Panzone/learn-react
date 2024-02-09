1c637451ff15b152a7cc4d223937b8799a0de4f6
Andrew-Panzone <panzone.an@northeastern.edu>
2024-02-09 16:58:22 -0500

    activities

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,22 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import ProfileMistake from './qcomps/profile_mistake'
+import FirstComp from './qcomps/firstcomp'
+import TodoList from "./qcomps/todos";
+import Profile from "./components/profile_props"
+import Gallery from "./qcomps/gallery_props"
+import Square from "./components/square"
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <ProfileMistake/>
+        <FirstComp/>
+        <TodoList/>
+        <Profile/>
+        <Gallery/>
+        <Square/>
     </div>
   )
 }
